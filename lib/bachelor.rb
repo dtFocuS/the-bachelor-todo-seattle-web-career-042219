125a1e8562f6fc69d746ecf03114d3aad77b4fa8
Danny Tseng <dtfocus1025@gmail.com>
2019-04-11 03:22:49 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,12 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  #code here
+  winner = ""
+  data[season].each do |contestant|
+    contestant.each do |category, value|
+      winner = contestant["name"] if value == "Winner"
+    end
+  end
+  winner.split()[0]
 end
 
 def get_contestant_name(data, occupation)
